e7c75825bcf0cf66d3e6845be7a61e6146372083
Marlon <marcomora61@gmail.com>
2020-08-09 18:27:24 -0500

    crenado variables en nuestro campo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,30 @@
  "use strict";
  document.addEventListener('DOMContentLoaded', function(){
 
-    console.log("listo");
+    //console.log("listo"); 
+
+    // campos datos usuarios 
+
+       var nombre=document.getElementById('nombre');
+       var apellido=document.getElementById('apellido');
+       var email=document.getElementById('email');
+    //campos pases 
+     
+     var pase_dia=document.getElementById('pase_dia')
+     var pase_dosdias=document.getElementById('pase_dosdias')
+     var pase_completo=document.getElementById('pase_completo')
+     
+     //botones y divs
+
+     var calcular=document.getElementById('calcular');
+     var errorDiv=document.getElementById('error');
+     var botonRegistro=document.getElementById('btnRegistro');
+     var resultado=document.getElementById('lista_productos');
+
+
+
+
+
 
 
  }); //DOM content loaded
