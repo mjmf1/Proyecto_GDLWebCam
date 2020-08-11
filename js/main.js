17c2f4c5494105f1305a201ceba123a5f4e556bc
Marlon <marcomora61@gmail.com>
2020-08-10 20:15:33 -0500

    habilitando el boton de calcular

--- js/main.js
+++ js/main.js
@@ -1,29 +1,50 @@
 (function(){
 
- "use strict";
- document.addEventListener('DOMContentLoaded', function(){
+   var regalo=document.getElementById('regalo');
+
+"use strict";
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
 
     //console.log("listo"); 
 
     // campos datos usuarios 
 
-       var nombre=document.getElementById('nombre');
-       var apellido=document.getElementById('apellido');
-       var email=document.getElementById('email');
+   var nombre=document.getElementById('nombre');
+   var apellido=document.getElementById('apellido');
+   var email=document.getElementById('email');
     //campos pases 
-     
-     var pase_dia=document.getElementById('pase_dia')
-     var pase_dosdias=document.getElementById('pase_dosdias')
-     var pase_completo=document.getElementById('pase_completo')
-     
+      
+   var pase_dia=document.getElementById('pase_dia')
+   var pase_dosdias=document.getElementById('pase_dosdias')
+   var pase_completo=document.getElementById('pase_completo')
+      
      //botones y divs
 
-     var calcular=document.getElementById('calcular');
-     var errorDiv=document.getElementById('error');
-     var botonRegistro=document.getElementById('btnRegistro');
-     var resultado=document.getElementById('lista_productos');
+   var calcular =document.getElementById('calcular');
+   var errorDiv =document.getElementById('error');
+   var botonRegistro=document.getElementById('btnRegistro');
+   var resultado=document.getElementById('lista_productos');
 
+   calcular.addEventListener('click', calcularMontos);
 
+   function calcularMontos(e){
+      e.preventDefault();
+   if(regalo.value===""){
+      alert("Debes elegir un regalo");
+      regalo.focus();
+   }else{
+      var boletosDia=pase_dia.value,
+          boleto2Dias=pase_dosdias.value,
+          boletoCompleto=pase_completo.value;
+
+          var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50);
+ console.log( totalPagar);
+   }
+
+}
 
 
 
