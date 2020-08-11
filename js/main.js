3caa9f83d3dd552fd06a553f2653bf4c70d86fb5
Marlon <marcomora61@gmail.com>
2020-08-10 20:34:54 -0500

    Calculos con JavaScript para cobrar según el boleto adquirido

--- js/main.js
+++ js/main.js
@@ -17,16 +17,20 @@ document.addEventListener('DOMContentLoaded', function(){
    var email=document.getElementById('email');
     //campos pases 
       
-   var pase_dia=document.getElementById('pase_dia')
-   var pase_dosdias=document.getElementById('pase_dosdias')
-   var pase_completo=document.getElementById('pase_completo')
+   var pase_dia= document.getElementById('pase_dia');
+   var pase_dosdias=document.getElementById('pase_dosdias');
+   var pase_completo=document.getElementById('pase_completo');
       
      //botones y divs
 
-   var calcular =document.getElementById('calcular');
-   var errorDiv =document.getElementById('error');
-   var botonRegistro=document.getElementById('btnRegistro');
-   var resultado=document.getElementById('lista_productos');
+   var calcular = document.getElementById('calcular');
+   var errorDiv = document.getElementById('error');
+   var botonRegistro = document.getElementById('btnRegistro');
+   var resultado = document.getElementById('lista_productos');
+
+   // extras
+    var camisas = document.getElementById('camisa_evento');
+   var etiquetas = document.getElementById('etiquetas');
 
    calcular.addEventListener('click', calcularMontos);
 
@@ -38,9 +42,11 @@ document.addEventListener('DOMContentLoaded', function(){
    }else{
       var boletosDia=pase_dia.value,
           boleto2Dias=pase_dosdias.value,
-          boletoCompleto=pase_completo.value;
+          boletoCompleto=pase_completo.value,
+          cantCamisas = camisas.value,
+          cantEtiquetas = etiquetas.value;
 
-          var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50);
+          var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10) *.93) + (cantEtiquetas * 2); 
  console.log( totalPagar);
    }
 
