09b21c88549d8a07e4ae680983c9aba600c827a8
Marlon <marcomora61@gmail.com>
2020-08-17 16:45:30 -0500

    Creando una función para Validar

--- js/main.js
+++ js/main.js
@@ -41,14 +41,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
    pase_completo.addEventListener('blur', mostarDias);
 
-   nombre.addEventListener('blur', function(){
+   nombre.addEventListener('blur', validarCampos);     //validacion
+   apellido.addEventListener('blur', validarCampos);
+   email.addEventListener('blur', validarCampos);
+
+   function validarCampos(){
     if (this.value == ''){
       errorDiv.style.display = 'block';
       errorDiv.innerHTML = 'Este campo es obligatorio';
       this.style.border = '1px solid red';
       errorDiv.style.border = '1px solid red';
-    };
-   });
+    } else {
+      errorDiv.style.display = 'none';
+      this.style.border = '1px solid #cccccc';
+    }
+
+   };
 
    function calcularMontos(e){
       e.preventDefault();
